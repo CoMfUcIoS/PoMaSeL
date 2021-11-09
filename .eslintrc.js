bedd32d99a14759de32507dbc80184bbecb63590
Ioannis Karasavvaidis <ioannis@puppet.com>
2021-11-09 17:31:33 +0000

    remove custom rules form eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,61 +22,7 @@ module.exports = {
     "eslint-plugin-import-helpers",
     "@typescript-eslint",
   ],
-  rules: {
-    "import/no-unresolved": "off",
-    "import/extensions": "off",
-    // note you must disable the base rule as it can report incorrect errors
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
-    "arrow-parens": ["error", "as-needed"],
-    "import/no-cycle": 0,
-    "import/no-named-as-default": 0,
-    "import/no-named-as-default-member": 0,
-    "import/prefer-default-export": 0,
-    "import-helpers/order-imports": [
-      "error",
-      {
-        newlinesBetween: "always",
-        groups: ["module", "/^~//", "/^@//"],
-        alphabetize: {
-          order: "asc",
-          ignoreCase: false,
-        },
-      },
-    ],
-    complexity: ["warn", 8],
-    "max-params": ["warn", 5],
-    "max-statements": ["warn", 15],
-    "max-statements-per-line": [
-      "error",
-      {
-        max: 1,
-      },
-    ],
-    "max-nested-callbacks": ["warn", 4],
-    "max-depth": [
-      "warn",
-      {
-        max: 3,
-      },
-    ],
-    "max-lines": ["warn", 400],
-    "prettier/prettier": [
-      "error",
-      {
-        singleQuote: false,
-        trailingComma: "all",
-        arrowParens: "avoid",
-      },
-    ],
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: ["**/*.test.js", "**/*.test.ts", "test/**"],
-      },
-    ],
-    "no-unused-expressions": 0,
-  },
+  rules: {},
   settings: {
     "import/resolver": {
       node: {
